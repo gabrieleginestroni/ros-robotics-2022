4e4940420be46ac9be0af62dbb97ed646abc18a6
gabrieleginestroni <gabrieleginestroni@gmail.com>
2022-04-15 12:01:53 +0200

    velocity computation module

--- pub_sub/src/sub.cpp
+++ pub_sub/src/sub.cpp
@@ -1,13 +1,19 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
-#include "std_msgs/Float64.h"
-#include "geometry_msgs/PoseStamped.h"
+#include "geometry_msgs/TwistStamped.h"
+
+#define N 42
+#define T 5
+#define PI 3.14159265358979323846
+#define R 0.07
+#define W 0.169
+#define L 0.2
+#define COEF 1/(W+L)
 
 class Subscriber {
     public:
         Subscriber(){
-            this->sub = this->n.subscribe("robot/pose", 1000, &Subscriber::compVelocity, this);
-            this->pub = this->n.advertise<std_msgs::Float64>("velocities",1000);
+            this->sub = this->n.subscribe("wheel_states", 1000, &Subscriber::compVelocity, this);
+            this->pub = this->n.advertise<geometry_msgs::TwistStamped>("cmd_vel",1000);
             this->count = 0;
         }
         void main_loop() {
@@ -17,41 +23,63 @@ class Subscriber {
                 loop_rate.sleep();
             }
         }
-        void compVelocity(const geometry_msgs::PoseStamped::ConstPtr& msg) {
-            float x_cur_pose,y_cur_pose;
-            float velocity;
+        void compVelocity(const sensor_msgs::JointState::ConstPtr& msg) {
             double cur_stamp;
-            float delta_x;
-            float delta_y;
-            std_msgs::Float64 vel_msg;
+            geometry_msgs::TwistStamped vel_msg_ticks;
+            geometry_msgs::TwistStamped vel_msg_rpm;
 
             if (this->count>0){
-              x_cur_pose = msg->pose.position.x;
-              y_cur_pose = msg->pose.position.y;
-              cur_stamp = msg->header.stamp.nsec;
-              delta_x = x_cur_pose-this->x_last_pose;
-              delta_y = y_cur_pose-this->y_last_pose;
-              velocity = sqrt(delta_x * delta_x + delta_y * delta_y) / ((cur_stamp -this->stamp)/ 1000000000);
-              ROS_INFO("Delta x: %f",delta_x);
-              ROS_INFO("Delta y: %f",delta_y);
-              ROS_INFO("Delta time: %lf",(cur_stamp -this->stamp)/ 1000000000);
-              vel_msg.data = velocity;
-              this->pub.publish(vel_msg);
+                float elapsed_time;
+                int delta_ticks[4];
+                float w_ticks[4];
+                float w_rpm[4];
+
+                if(msg->header.stamp.sec > this->stamp_sec)
+                    elapsed_time = ((1000000000 - this->stamp_nsec) + msg->header.stamp.nsec) / 1000000000;
+                else
+                    elapsed_time = (msg->header.stamp.nsec - this->stamp_nsec) / 1000000000;
+
+                for (int i = 0; i < 4; i++) {
+                    delta_ticks[i] = msg->position[i] - this->wheels_ticks_old[i];
+                    w_ticks[i] = (delta_ticks[i] / elapsed_time) * (2 * PI) / (N * T);
+                    w_rpm[i] = msg->velocity[i] / T;
+                }
+
+                vel_msg_ticks.twist.linear.x = (R / 4) * (w_ticks[0] + w_ticks[1] + w_ticks[2] + w_ticks[3]);
+                vel_msg_ticks.twist.linear.y = (R / 4) * (w_ticks[1] - w_ticks[0] + w_ticks[2] - w_ticks[3]);
+                vel_msg_ticks.twist.linear.z = 0.0;
+                vel_msg_ticks.twist.angular.x = 0.0;
+                vel_msg_ticks.twist.angular.y = 0.0;
+                vel_msg_ticks.twist.angular.z = (R / 4) * (w_ticks[1] + w_ticks[3] - w_ticks[0] - w_ticks[2]) * COEF;
+
+                this->pub.publish(vel_msg_ticks);
+
+                vel_msg_rpm.twist.linear.x = (R / 4) * (w_rpm[0] + w_rpm[1] + w_rpm[2] + w_rpm[3]);
+                vel_msg_rpm.twist.linear.y = (R / 4) * (w_rpm[1] - w_rpm[0] + w_rpm[2] - w_rpm[3]);
+                vel_msg_rpm.twist.linear.z = 0.0;
+                vel_msg_rpm.twist.angular.x = 0.0;
+                vel_msg_rpm.twist.angular.y = 0.0;
+                vel_msg_rpm.twist.angular.z = (R / 4) * (w_rpm[1] + w_rpm[3] - w_rpm[0] - w_rpm[2]) * COEF;
+
+                this->pub.publish(vel_msg_rpm);
+
             }
+
+            for (int i = 0; i < 4; i++)
+                this->wheels_ticks_old[i] = msg->position[i];
             this->count++;
-            this->stamp = msg->header.stamp.nsec;
-            this->x_last_pose = msg->pose.position.x;
-            this->y_last_pose = msg->pose.position.y;
+            this->stamp_nsec = msg->header.stamp.nsec;
+            this->stamp_sec = msg->header.stamp.sec;
         }
 
     private:
         ros::NodeHandle n;
         ros::Subscriber sub;
         ros::Publisher pub;
+        int wheels_ticks_old[4];
+        int stamp_sec;
+        int stamp_nsec;
         int count;
-        double stamp;
-        float x_last_pose;
-        float y_last_pose;
 
 };
 
