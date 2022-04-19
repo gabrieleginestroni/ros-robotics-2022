1f4f4208abb55e645ae93e67beccf9aa8b1277ba
tommyknocker <tommasocapacci@gmail.com>
2022-04-19 12:36:18 +0200

    first odometry implementation

---
+++ project1/src/odom_pub.cpp
@@ -0,0 +1,100 @@
+#include "ros/ros.h"
+#include "geometry_msgs/TwistStamped.h"
+#include "nav_msgs/Odometry.h"
+
+#include <math.h>
+#include <tf/transform_broadcaster.h>
+
+enum class integrate_type {EULER, RUNGE_KUTTA};
+
+class Odom_pub{
+    public:
+        Odom_pub(){
+            this->sub = this->n.subscribe("cmd_vel", 1000, &Odom_pub::compOdometry, this);
+            this->pub = this->n.advertise<nav_msgs::Odometry>("odom",1000);
+            this->type = integrate_type::EULER;
+
+            this->n.getParam("/initial_pose_x", this->x_old);
+            this->n.getParam("/initial_pose_j", this->y_old);
+            this->n.getParam("/initial_pose_theta", this->theta_old);
+        }
+
+        void main_loop() {
+            ros::Rate loop_rate(10);
+            while(ros::ok()){
+                ros::spinOnce();
+                loop_rate.sleep();
+            }
+        }
+
+        void compOdometry(const geometry_msgs::TwistStamped::ConstPtr& msg){
+            double x_new, y_new, theta_new, time_s;
+            ros::Duration elapsed_time;
+            nav_msgs::Odometry msg_odometry;
+
+            elapsed_time = msg->header.stamp - this->stamp;
+            time_s = elapsed_time.toSec();
+
+            if (this->type == integrate_type::EULER) {
+                x_new = this->x_old + (msg->twist.linear.x * cos(theta_old) - msg->twist.linear.y * sin(theta_old)) * time_s;
+                y_new = this->y_old + (msg->twist.linear.x * sin(theta_old) + msg->twist.linear.y * cos(theta_old)) * time_s;
+            } else {
+                x_new = this->x_old +
+                        (msg->twist.linear.x * cos(theta_old + msg->twist.angular.z * time_s * 0.5) -
+                         msg->twist.linear.y * sin(theta_old + msg->twist.angular.z * time_s * 0.5)) * time_s;
+                y_new = this->y_old +
+                        (msg->twist.linear.x * sin(theta_old + msg->twist.angular.z * time_s * 0.5) +
+                         msg->twist.linear.y * cos(theta_old + msg->twist.angular.z * time_s * 0.5)) * time_s;
+            }
+
+            theta_new = this->theta_old + msg->twist.angular.z * time_s;
+
+            msg_odometry.header = msg->header;
+            msg_odometry.header.frame_id = "odom";
+
+            msg_odometry.child_frame_id = "base_link";
+
+            msg_odometry.pose.pose.position.x = x_new;
+            msg_odometry.pose.pose.position.y = y_new;
+            msg_odometry.pose.pose.position.z = 0.0;
+            msg_odometry.pose.pose.orientation = tf::createQuaternionMsgFromYaw(theta_new);
+
+            msg_odometry.twist.twist = msg->twist;
+
+            /*
+            for (int i = 0; i < 36; i++) {
+                msg_odometry.odom.pose.covariance[i] = 0.0;
+                msg_odometry.odom.twist.covariance[i] = 0.0;
+            }
+            */
+
+            this->pub.publish(msg_odometry);
+
+            this->x_old = x_new;
+            this->y_old = y_new;
+            this->theta_old = theta_new;
+            this->stamp = msg->header.stamp;
+
+        }
+
+    private:
+        //all services missing
+        ros::NodeHandle n;
+        ros::Subscriber sub;
+        ros::Publisher pub;
+        ros::Time stamp;
+        integrate_type type;
+        double x_old;
+        double y_old;
+        double theta_old;
+        int count;
+        tf::TransformBroadcaster transform_broadcaster;
+        tf::Transform transform;
+};
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "odom_pub");
+    Odom_pub odomPub;
+    odomPub.main_loop();
+    return 0;
+}
--- project1/src/vel_pub.cpp
+++ project1/src/vel_pub.cpp
@@ -2,7 +2,6 @@
 #include "sensor_msgs/JointState.h"
 #include "geometry_msgs/TwistStamped.h"
 
-#define B 1000000000
 #define N 42
 #define T 5
 #define PI 3.14159265358979323846
@@ -10,14 +9,17 @@
 #define W 0.169
 #define L 0.2
 
-class Subscriber {
+enum class vel_type {RPM, TICKS};
+
+class Vel_pub {
     public:
-        Subscriber(){
-            this->sub = this->n.subscribe("wheel_states", 1000, &Subscriber::compVelocity, this);
-            this->pub_ticks = this->n.advertise<geometry_msgs::TwistStamped>("cmd_vel_ticks",1000);
-            this->pub_rpm = this->n.advertise<geometry_msgs::TwistStamped>("cmd_vel_rpm",1000);
+        Vel_pub(){
+            this->sub = this->n.subscribe("wheel_states", 1000, &Vel_pub::compVelocity, this);
+            this->pub = this->n.advertise<geometry_msgs::TwistStamped>("cmd_vel",1000);
             this->count = 0;
+            this->type = vel_type::RPM;
         }
+
         void main_loop() {
             ros::Rate loop_rate(10);
             while(ros::ok()){
@@ -25,7 +27,9 @@ class Subscriber {
                 loop_rate.sleep();
             }
         }
+
         void compVelocity(const sensor_msgs::JointState::ConstPtr& msg) {
+            //we could encapsulate all the computation inside an if...else
             geometry_msgs::TwistStamped vel_msg_ticks;
             geometry_msgs::TwistStamped vel_msg_rpm;
 
@@ -36,14 +40,11 @@ class Subscriber {
                 double w_ticks[4];
                 double w_rpm[4];
 
-                elapsed_time = msg->header.stamp - stamp;
+                elapsed_time = msg->header.stamp - this->stamp;
                 time_s = elapsed_time.toSec();
-
-                ROS_INFO("elapsed time: %f\n", time_s);
 
                 for (int i = 0; i < 4; i++) {
                     delta_ticks[i] = msg->position[i] - this->wheels_ticks_old[i];
-                    ROS_INFO("delta ticks: %f\n", delta_ticks[i]);
                     w_ticks[i] = (delta_ticks[i] / time_s) * (2 * PI ) / (N * T);
                     w_rpm[i] = msg->velocity[i] / (60 * T);
                 }
@@ -55,8 +56,6 @@ class Subscriber {
                 vel_msg_ticks.twist.angular.y = 0.0;
                 vel_msg_ticks.twist.angular.z = (R / 4) * (w_ticks[1] + w_ticks[3] - w_ticks[0] - w_ticks[2]) / (L+W);
 
-                this->pub_ticks.publish(vel_msg_ticks);
-
                 vel_msg_rpm.twist.linear.x = (R / 4) * (w_rpm[0] + w_rpm[1] + w_rpm[2] + w_rpm[3]);
                 vel_msg_rpm.twist.linear.y = (R / 4) * (w_rpm[1] - w_rpm[0] + w_rpm[2] - w_rpm[3]);
                 vel_msg_rpm.twist.linear.z = 0.0;
@@ -64,7 +63,10 @@ class Subscriber {
                 vel_msg_rpm.twist.angular.y = 0.0;
                 vel_msg_rpm.twist.angular.z = (R / 4) * (w_rpm[1] + w_rpm[3] - w_rpm[0] - w_rpm[2]) / (L+W);
 
-                this->pub_rpm.publish(vel_msg_rpm);
+                if(this->type == vel_type::RPM)
+                    this->pub.publish(vel_msg_rpm);
+                else
+                    this->pub.publish(vel_msg_ticks);
 
             }
 
@@ -78,18 +80,18 @@ class Subscriber {
     private:
         ros::NodeHandle n;
         ros::Subscriber sub;
-        ros::Publisher pub_ticks;
-        ros::Publisher pub_rpm;
-        double wheels_ticks_old[4];
+        ros::Publisher pub;
         ros::Time stamp;
+        vel_type type;
+        double wheels_ticks_old[4];
         int count;
 
 };
 
 int main(int argc, char **argv) {
-    ros::init(argc, argv, "sub");
-    Subscriber my_sub;
-    my_sub.main_loop();
+    ros::init(argc, argv, "vel_pub");
+    Vel_pub velPub;
+    velPub.main_loop();
     return 0;
 
 }
