e69d881b2591b25372f255d0ff9fa70c3ca95caa
gabrieleginestroni <gabrieleginestroni@gmail.com>
2022-04-27 17:17:09 +0200

    upload calibration csv and bags

--- project1/python/bag_to_csv.py
+++ project1/python/bag_to_csv.py
@@ -3,16 +3,16 @@
 import rosbag
 import csv
 
-bag = rosbag.Bag('../bags/calibration3.bag')
+bag = rosbag.Bag('../bags/calibration1.bag')
 
-f = open('../csv/calibration3.csv', 'w')
+f = open('../csv/calibration1.csv', 'w')
 writer = csv.writer(f)
 
-header = ['timestamp', 'x', 'y','fl','fr', 'rl', 'rr']
+header = ['sec', 'nsec', 'x', 'y','fl','fr', 'rl', 'rr']
 writer.writerow(header)
 
 for topic, msg, t in bag.read_messages(topics=['/pose_vel_sync']):
-    writer.writerow([t, msg.poseX, msg.poseY, msg.rpm_fl, msg.rpm_fr, msg.rpm_rl, msg.rpm_rr])
+    writer.writerow([msg.sec, msg.nsec, msg.poseX, msg.poseY, msg.rpm_fl, msg.rpm_fr, msg.rpm_rl, msg.rpm_rr])
 
 bag.close()
 f.close()
