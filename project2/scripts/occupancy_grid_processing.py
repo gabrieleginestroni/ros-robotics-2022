ae412d7f9c54b9af3e53f3947fb607a91f4ee474
gabrieleginestroni <gabrieleginestroni@gmail.com>
2022-06-10 00:15:00 +0200

    parameters tuning

--- project2/scripts/occupancy_grid_processing.py
+++ project2/scripts/occupancy_grid_processing.py
@@ -4,6 +4,7 @@ import numpy as np
 import cv2
 from nav_msgs.msg import OccupancyGrid
 
+
 pub = rospy.Publisher('my_map', OccupancyGrid, queue_size=1, latch=True)
 
 def callback(data):
@@ -17,7 +18,9 @@ def callback(data):
     for p in data.data:
       image[counter%width][int(counter/width)] = p
       counter+=1
-
+    
+    
+    
     kernel = np.ones((2,2),np.uint8)
     image = cv2.dilate(image,kernel,iterations = 1)
     image = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
@@ -28,23 +31,35 @@ def callback(data):
     custom_map.data =  np.transpose(image).flatten().astype (int)
     pub.publish(custom_map)
 
+
+
     image = cv2.rotate(image, cv2.ROTATE_180)
     image[image == -1] = 150
     image[image == 0] = 255
     image[image == 100] = 0
-
-    '''
+    
     filename = '../maps/grid.png'
     print ("saving image")
     cv2.imwrite (filename,image)
-    '''
 
+
+    
+
+ 
+  
 
 def listener():
-    rospy.init_node('depth_saver', anonymous=False)
-    rospy.Subscriber("/map", OccupancyGrid, callback)
-    rospy.spin()
 
+    rospy.init_node('depth_saver', anonymous=False)
+
+    rospy.Subscriber("/map", OccupancyGrid, callback)
+    
+
+    
+    
+    
+   
+    rospy.spin()
 
 if __name__ == '__main__':
     listener()
