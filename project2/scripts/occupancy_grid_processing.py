aa81dbf432c83260960a7021dc9725d031852cc0
tommyknocker <tommasocapacci@gmail.com>
2022-06-10 11:02:49 +0200

    reverted to the use of the provided odometry and starting localization

--- project2/scripts/occupancy_grid_processing.py
+++ project2/scripts/occupancy_grid_processing.py
@@ -7,6 +7,7 @@ from nav_msgs.msg import OccupancyGrid
 
 pub = rospy.Publisher('my_map', OccupancyGrid, queue_size=1, latch=True)
 
+
 def callback(data):
     width = data.info.width
     height = data.info.height
@@ -14,12 +15,10 @@ def callback(data):
     image = np.zeros(size)
     
     counter = 0
-    #print (len(data.data))
+    # print (len(data.data))
     for p in data.data:
-      image[counter%width][int(counter/width)] = p
-      counter+=1
-    
-    
+        image[counter%width][int(counter/width)] = p
+        counter+=1
     
     kernel = np.ones((2,2),np.uint8)
     image = cv2.dilate(image,kernel,iterations = 1)
@@ -31,35 +30,23 @@ def callback(data):
     custom_map.data =  np.transpose(image).flatten().astype (int)
     pub.publish(custom_map)
 
-
-
     image = cv2.rotate(image, cv2.ROTATE_180)
     image[image == -1] = 150
     image[image == 0] = 255
     image[image == 100] = 0
-    
+
+    '''
     filename = '../maps/grid.png'
     print ("saving image")
     cv2.imwrite (filename,image)
+    '''
 
-
-    
-
- 
-  
 
 def listener():
-
     rospy.init_node('depth_saver', anonymous=False)
-
     rospy.Subscriber("/map", OccupancyGrid, callback)
-    
-
-    
-    
-    
-   
     rospy.spin()
+
 
 if __name__ == '__main__':
     listener()
